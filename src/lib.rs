e9c4374e21d5c8a0372df0e528c086b85f51c377
Darethan025 <kigangadarell@gmail.com>
2025-08-26 20:27:23 +0300

    Operators implemented in an impl block with defined fields

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,38 @@
-pub fn greet() {
-    println!("\n\nHello, you just called the C4lc public function greet() and to use the public struct Operators, you can do this let variablename = C4lc::Operators, and add the missing fields to use the struct for your mathematical operations\n\n");
-}
-
 #[derive(Debug)]
+
+/// A struct containing basic math operators.
+///
+/// # Examples
+///
+/// ```
+/// use c4lc::Operators;
+///
+/// fn main() {
+///     let ops = Operators::new();
+///     let add = ops.addition;
+/// }
+/// 
+
 pub struct Operators {
     pub addition: char,
     pub subtraction: char,
     pub multiplication: char,
     pub division: char,
+    pub modulo: char,
+}
+
+/// There's a struct declared with the required fields to be accessed
+/// They're public and can be accessed in project directory if the crate is added to your project
+/// Only the symbols defined are the ones tied to the fields
+
+impl Operators {
+    pub fn new() -> Self {
+        Self {
+            addition: '+',
+            subtraction: '-',
+            multiplication: '*',
+            division: '/',
+            modulo: '%',
+        }
+    }
 }
